ba5b5a516f15d8bbfa276ad6517d84aac586475f
gezhihao <911766388@qq.com>
2019-09-17 15:15:04 +0800

    init package

---
+++ dist/js-plugin-template.js
@@ -0,0 +1,17 @@
+/**
+ * js-plugin-template v0.0.1 By gezhihao
+ * Github: https://github.com/gehaollt/js-plugin-template
+ * MIT Licensed.
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global['js-plugin-template'] = factory());
+}(this, (function () { 'use strict';
+
+var index = {};
+
+return index;
+
+})));
